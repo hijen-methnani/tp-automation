6e27dc95bc5226e6d22e36500193f1d988febdc6
Hijen METHNANI <hijenmethnani1@gmail.com>
2025-11-05 11:13:28 +0100

    Update update_system.sh

--- scripts/update_system.sh
+++ scripts/update_system.sh
@@ -11,18 +11,18 @@ echo "=== MISE À JOUR DU SYSTÈME ==="
 # 1. Met à jour la liste des paquets
 echo "1. Mise à jour liste des paquets..."
 if sudo $UPDATE_CMD; then
-    echo "✓ Succès"
+    echo "Succès"
 else
-    echo "✗ Échec"
+    echo "Échec"
     exit 1
 fi
 
 # 2. Met à jour tous les paquets
 echo "2. Mise à jour des paquets..."
 if sudo $UPGRADE_CMD; then
-    echo "✓ Succès"
+    echo "Succès"
 else
-    echo "✗ Échec"
+    echo " Échec"
     exit 1
 fi
 
