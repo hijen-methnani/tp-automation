efed1aa11f3dd8456886e7ab7ab60f13355b1d2b
Hijen METHNANI <hijenmethnani1@gmail.com>
2025-11-05 15:55:05 +0100

    Update network_diag.sh

--- scripts/network_diag.sh
+++ scripts/network_diag.sh
@@ -3,7 +3,6 @@
 source ../config/config.cfg
 
 echo "1. Test Internet ($TEST_HOST)"
-ping -c 2 $TEST_HOST &> /dev/null && echo " Internet " || echo "No Internet"
 
 echo "2. Ports ouverts :"
 netstat -tuln | grep LISTEN
